fc06d0bdbf9f849e1ff43dde55632f5c11a2a1e3
shakhard <david.shakhar@techlib.cz>
2021-11-08 14:29:44 +0100

    better functions

--- main.py
+++ main.py
@@ -1,54 +1,62 @@
 from art import logo, vs
 from game_data import data
-from random import randint
+from random import choice
 import os
+
 
 def clear():
     return os.system('clear')
 
 
-def pick_item():
-    """Picks random item from list"""
-    random_index = randint(0, len(data) - 1)
-    return data[random_index]
+def format_data(account):
+    """Takes the account data and returns printable format"""
+    account_name = account['name']
+    account_description = account['description']
+    account_country = account['country']
+    return f"{account_name}, a {account_description}, from {account_country}"
 
-def process_correct_answer(score):
-    score += 1
-    print(f"You are right! Current score: {score}")
-    return score
-            
+
+def check_answer(guess, follower_count_a, follower_count_b):
+    """Take the user guess and follower counts and returns if they got it right"""
+    if follower_count_a > follower_count_b:
+        return guess == "a"  # True id a, False if b
+    else:
+        return guess == "b"
+
+
 def game():
-    item_a = pick_item()
+    item_b = choice(data)
     score = 0
     continue_game = True
+
     while continue_game:
-        print(f"Compare A: {item_a['name']}, a {item_a['description']}, from {item_a['country']}")
-        item_b = pick_item()
-        
+        item_a = item_b
+        item_b = choice(data)
+
         while item_a == item_b:
-            item_b = pick_item()
-        
+            item_b = choice(data)
+
+        print(f"Compare A: {format_data(item_a)}")
         print(vs)
-        
-        print(f"Against B: {item_b['name']}, a {item_b['description']}, from {item_b['country']}")
-        player_guess = input("Who has more followers? Type 'A' or 'B': ").upper()
-        
-        clear()
-        
+        print(f"Against B: {format_data(item_b)}")
+
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
         follower_count_a = item_a['follower_count']
         follower_count_b = item_b['follower_count']
-        
+        is_correct = check_answer(guess, follower_count_a, follower_count_b)
+
+        clear()
         print(logo)
-        
-        if (follower_count_a > follower_count_b and player_guess == "A") or (follower_count_a < follower_count_b and player_guess == "B"):
-            score = process_correct_answer(score)
-            item_a = item_b
+
+        if is_correct:
+            score += 1
+            print(f"You are right! Current score: {score}")
+
         else:
             print(f"Sorry, that's wrong. Final score: {score}")
             continue_game = False
 
+
 print(logo)
 game()
-
-
-
