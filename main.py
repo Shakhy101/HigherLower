58ca439d7194a3e16bfbd12df0125295a238307a
shakhard <david.shakhar@techlib.cz>
2021-11-08 13:23:03 +0100

    functions

--- main.py
+++ main.py
@@ -1,57 +1,53 @@
 from art import logo, vs
 from game_data import data
 from random import randint
+import os
 
-# vyber nahodny dict z listu - nejprve nahodne cislo od 0 do len(list -1)
+def clear():
+    return os.system('clear')
+
+
 def pick_item():
+    """Picks random item from list"""
     random_index = randint(0, len(data) - 1)
     return data[random_index]
-    
 
-
+def process_correct_answer(score):
+    score += 1
+    print(f"You are right! Current score: {score}")
+    return score
+            
 def game():
-# na zacatku potrebuji vybrat 2 itemy
     item_a = pick_item()
-
-    
     score = 0
     continue_game = True
     while continue_game:
-# vytisknout Compare A Against B
         print(f"Compare A: {item_a['name']}, a {item_a['description']}, from {item_a['country']}")
         item_b = pick_item()
         
+        while item_a == item_b:
+            item_b = pick_item()
+        
+        print(vs)
+        
         print(f"Against B: {item_b['name']}, a {item_b['description']}, from {item_b['country']}")
-
-        # ziskat guess od uzivatele:
         player_guess = input("Who has more followers? Type 'A' or 'B': ").upper()
-
-        # analyzuj co vybral uzivatel a podle toho urcim 1 - pocet followeru co vybral uzivatel vs pocet followeru druheho listu
-        if player_guess == "A":
-            follower_count_player = item_a['follower_count']
-            follower_count_computer = item_b['follower_count']
-        else:
-            follower_count_player = item_b['follower_count']
-            follower_count_computer = item_a['follower_count']
-
-        print(f"follower by player: {follower_count_player}")
-        print(f"follower by computer: {follower_count_computer}")
-
-        # urci kdo ma pravdu
-
-        if follower_count_player > follower_count_computer:
-            #you are right, pripocti bod
-            score += 1
-            print(f"You are right! Current score: {score}")
+        
+        clear()
+        
+        follower_count_a = item_a['follower_count']
+        follower_count_b = item_b['follower_count']
+        
+        print(logo)
+        
+        if (follower_count_a > follower_count_b and player_guess == "A") or (follower_count_a < follower_count_b and player_guess == "B"):
+            score = process_correct_answer(score)
             item_a = item_b
         else:
-            # wrong, print final score, game over
             print(f"Sorry, that's wrong. Final score: {score}")
             continue_game = False
-        
 
-        
-
+print(logo)
 game()
 
 
